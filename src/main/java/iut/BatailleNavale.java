689b976736afaa6d19e78aa7819c8a191387db78
Luc Mazardo <luc@mazardo.com>
2015-12-02 23:39:51 +0100

    Migrating from legacy to container-based infrastructure

--- src/main/java/iut/BatailleNavale.java
+++ src/main/java/iut/BatailleNavale.java
@@ -1,11 +1,5 @@
 package iut;
 
 public class BatailleNavale {
-    public boolean isRunning() {
-        return false;
-    }
 
-    public static void main(String[] args) {
-        System.out.println("bataille navale");
-    }
 }
