ad6c5cb669667eb5d9ff093eb57faec6061e5403
Luc Mazardo <luc@mazardo.com>
2015-12-02 23:43:34 +0100

    green test

--- src/main/java/iut/BatailleNavale.java
+++ src/main/java/iut/BatailleNavale.java
@@ -2,4 +2,51 @@ package iut;
 
 public class BatailleNavale {
 
+    public boolean isFinished() {
+        return hasSurrender;
+    }
+
+    public void surrender() {
+        hasSurrender=true;
+    }
+
+    public void sinkBoat() {
+        nbBoat--;
+    }
+
+    public void play(String player) {
+        this.player = player;
+    }
+
+    public String lastPlayed() {
+        return player;
+    }
+
+    public int getNbBoat() {
+            return nbBoat;
+    }
+
+    public void addBoat() {
+        nbBoat++;
+    }
+
+    public boolean isInitialize() {
+        return true;
+    }
+
+    public boolean haveBoats() {
+
+        return getNbBoat()>0;
+    }
+
+    public void initBoat() {
+        //haveBoat = true;
+
+        nbBoat = 5;
+        //return haveBoat;
+    }
+
+    private boolean hasSurrender = false;
+    private String player;
+    int nbBoat = 0;
 }
--- src/test/java/iut/BatailleNavaleTest.java
+++ src/test/java/iut/BatailleNavaleTest.java
@@ -1,18 +1,91 @@
 package iut;
 
-import iut.BatailleNavale;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BatailleNavaleTest {
 
-
     @Test
-    public void verifyBatailleNavaleIsNotLaunched() {
-
+    public void canInitialize() {
         BatailleNavale batailleNavale = new BatailleNavale();
 
-        assertEquals(false, batailleNavale.isRunning());
+        boolean initialize = batailleNavale.isInitialize();
+        assertEquals(true, initialize);
     }
+
+    @Test
+    public void shouldHaveBoats() {
+        BatailleNavale batailleNavale2 = new BatailleNavale();
+
+        assertEquals(false, batailleNavale2.haveBoats());
+    }
+
+    @Test
+    public void shouldHaveBoatTestAfterInit() {
+        BatailleNavale batNav = new BatailleNavale();
+        batNav.initBoat();
+        assertEquals(true, batNav.haveBoats());
+    }
+
+    @Test
+    public void checkIfbattleIsNotFinished() {
+        boolean hasFinished = new BatailleNavale().isFinished();
+        assertFalse(hasFinished);
+
+    }
+
+    @Test
+    public void checkIfBattleIsFinishedWhenSurrendering() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+
+        batailleNavale.surrender();
+        boolean finished = batailleNavale.isFinished();
+        assertTrue(finished);
+    }
+
+    @Test
+    public void shouldHaveFiveBoat() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+        batailleNavale.initBoat();
+        assertEquals(5, batailleNavale.getNbBoat());
+    }
+
+    @Test
+    public void AddBoat() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+        batailleNavale.initBoat();
+        batailleNavale.addBoat();
+        assertEquals(6, batailleNavale.getNbBoat());
+    }
+
+    @Test
+    public void canSinkBoat() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+        batailleNavale.initBoat();
+        batailleNavale.addBoat();//6 bateaux
+        batailleNavale.sinkBoat();
+
+        assertEquals(5, batailleNavale.getNbBoat());
+
+    }
+
+    @Test
+    public void playerOneCouldPlay() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+
+        batailleNavale.play("playerOne");
+
+        assertEquals("playerOne", batailleNavale.lastPlayed());
+    }
+
+    @Test
+    public void playerTwoCouldPlay() {
+        BatailleNavale batailleNavale = new BatailleNavale();
+
+        batailleNavale.play("playerTwo");
+
+        assertEquals("playerTwo", batailleNavale.lastPlayed());
+    }
+
 }
